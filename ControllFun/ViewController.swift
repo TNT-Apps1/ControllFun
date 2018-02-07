f3637dd020f8efd74fd770b6fa4d242394909de6
Thompson, Grant H <thompsgh@mm570-258455mac.ad.matc.edu>
2018-02-07 14:43:25 -0600

    1st Commit

--- ControllFun/ViewController.swift
+++ ControllFun/ViewController.swift
@@ -9,17 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        sliderLabel.text = "50"
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var nameField: UITextField!
+    
+    @IBOutlet weak var numberField: UITextField!
+    
+    @IBOutlet weak var sliderLabel: UILabel!
+    
 
+    @IBOutlet var Tap_Gesture_Responder: UITapGestureRecognizer!
+    
+    
+    @IBAction func hideFirstResponders(_ sender: UITapGestureRecognizer) {
+        nameField.resignFirstResponder()
+        numberField.resignFirstResponder()
+        
+        
+    }
 }
 
